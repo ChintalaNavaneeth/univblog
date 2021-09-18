d0acdf9f095a569207f3ef3a5c2132935fce5076
ChintalaNavaneeth <45870912+ChintalaNavaneeth@users.noreply.github.com>
2021-09-18 19:20:03 +0530

    Update settings.py

--- univ_blog/settings.py
+++ univ_blog/settings.py
@@ -180,8 +180,8 @@ SITE_ID = 3
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'univblog0404@gmail.com'
-EMAIL_HOST_PASSWORD = 'thisisunivblogpassword'
+EMAIL_HOST_USER = 'enter your email'
+EMAIL_HOST_PASSWORD = 'Enter your password'
 EMAIL_USE_TLS = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
